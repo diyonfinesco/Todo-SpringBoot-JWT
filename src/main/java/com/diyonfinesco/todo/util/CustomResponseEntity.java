5fbb02d3344fd47f32bdecd5913a894253b2ef3e
diyonfinesco <chanaka.s@icloud.com>
2024-02-16 11:23:53 +0100

    add lombook annotations for CustomResponseEntity

--- src/main/java/com/diyonfinesco/todo/util/CustomResponseEntity.java
+++ src/main/java/com/diyonfinesco/todo/util/CustomResponseEntity.java
@@ -1,41 +1,14 @@
 package com.diyonfinesco.todo.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CustomResponseEntity {
     private int status;
     private boolean success;
     private Object result;
-
-    public CustomResponseEntity() {
-
-    }
-
-    public CustomResponseEntity(int status, boolean success, Object result) {
-        this.status = status;
-        this.success = success;
-        this.result = result;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public Object getResult() {
-        return result;
-    }
-
-    public void setResult(Object result) {
-        this.result = result;
-    }
 }
