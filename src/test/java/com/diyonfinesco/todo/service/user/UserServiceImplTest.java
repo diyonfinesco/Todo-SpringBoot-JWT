f006ed8ea46adeaf611c56e7ba8e152820dec7a9
Chanaka Subasinghage <chanaka.s@icloud.com>
2024-03-16 00:50:38 +0100

    add test cases for findById and findByEmail

--- src/test/java/com/diyonfinesco/todo/service/user/UserServiceImplTest.java
+++ src/test/java/com/diyonfinesco/todo/service/user/UserServiceImplTest.java
@@ -9,6 +9,7 @@ import com.diyonfinesco.todo.util.CustomResponse;
 import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -69,14 +71,72 @@ class UserServiceImplTest {
         assertThat(response.getResult()).usingRecursiveComparison().isEqualTo(usersDTOList);
     }
 
-    @Test
-    @Disabled
-    void findById() {
+    @Nested
+    class findById {
+
+        @Test
+        void itShouldReturnResponseWithUser() {
+            // arrange
+            UserEntity userEntity = new UserEntity("1", "john@example.com", "password", Role.ROLE_USER);
+            ReturnUserDTO userDTO = new ReturnUserDTO("1", "john@example.com");
+
+            when(userRepository.findById("65f419007e3ed7228a35a8cd")).thenReturn(Optional.of(userEntity));
+            when(returnUserMapper.toDTO(userEntity)).thenReturn(userDTO);
+
+            // act
+            CustomResponse response = userService.findById("65f419007e3ed7228a35a8cd");
+
+            // assert
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.isSuccess()).isTrue();
+            assertThat(response.getResult()).isEqualTo(userDTO);
+        }
+
+        @Test
+        void itShouldReturnResponseWithNotFound() {
+            // act
+            CustomResponse response = userService.findById("a");
+
+            // assert
+            assertThat(response.getStatus()).isEqualTo(404);
+            assertThat(response.isSuccess()).isFalse();
+            assertThat(response.getResult()).isEqualTo("User not found!");
+        }
     }
 
-    @Test
-    @Disabled
-    void findByEmail() {
+    @Nested
+    class findByEmail {
+
+        @Test
+        void itShouldReturnResponseWithUser() {
+            // arrange
+            String email = "john@example.com";
+
+            UserEntity userEntity = new UserEntity("1", email, "password", Role.ROLE_USER);
+            ReturnUserDTO userDTO = new ReturnUserDTO("1", "john@example.com");
+
+            when(userRepository.findByEmail(email)).thenReturn(Optional.of(userEntity));
+            when(returnUserMapper.toDTO(userEntity)).thenReturn(userDTO);
+
+            // act
+            CustomResponse response = userService.findByEmail(email);
+
+            // assert
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.isSuccess()).isTrue();
+            assertThat(response.getResult()).isEqualTo(userDTO);
+        }
+
+        @Test
+        void itShouldReturnResponseWithNotFound() {
+            // act
+            CustomResponse response = userService.findByEmail("john@example.com");
+
+            // assert
+            assertThat(response.getStatus()).isEqualTo(404);
+            assertThat(response.isSuccess()).isFalse();
+            assertThat(response.getResult()).isEqualTo("User not found!");
+        }
     }
 
     @Test
